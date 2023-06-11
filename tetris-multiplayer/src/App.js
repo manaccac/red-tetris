21dfd6bc0f72db42147486cf0853e4eb78dbd245
root <root@DESKTOP-A261UJI>
2023-06-11 23:16:16 +0200

    added node server without CORS error

--- tetris-multiplayer/src/App.js
+++ tetris-multiplayer/src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Game from './components/Game/Game';
 import Home from './components/Home/Home';
+import io from 'socket.io-client';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    const socket = io('http://localhost:3001');
+
+    socket.on('connect', () => {
+      socket.emit('client connected', 'Hello server!');
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return (
     <Router>
       <Routes>
