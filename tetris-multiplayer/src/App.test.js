932fd4f4f96b66138e128e1498f56ba1a5015f41
manaccac <maximenaccache@gmail.com>
2023-07-21 16:21:16 +0200

    avancer dans les tests

--- tetris-multiplayer/src/App.test.js
+++ tetris-multiplayer/src/App.test.js
@@ -1,27 +1,26 @@
 /** @jsxRuntime classic */
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import React from 'react';
 import App from './App';
 import UsernamePrompt from './components/UsernamePrompt';
-
 
 jest.mock('./App.css', () => ({}));
 
 test('Renders App component', () => {
   render(<App />);
 });
+
 jest.mock('js-cookie', () => ({
-	get: jest.fn(),
+  get: jest.fn(),
 }));
 
-  test('Renders UsernamePrompt when username is empty', () => {
-	const { getByTestId } = render(<UsernamePrompt />);
-	const usernamePrompt = getByTestId('usernamePrompt');
-	expect(usernamePrompt).toBeInTheDocument();
-  });
-  
+test('Renders UsernamePrompt when username is empty', () => {
+  const { getByTestId } = render(<UsernamePrompt />);
+  const usernamePrompt = getByTestId('usernamePrompt');
+  expect(usernamePrompt).toBeInTheDocument();
+});
 
 jest.mock('js-cookie', () => ({
-	get: jest.fn().mockReturnValue('TestUser'),
+  get: jest.fn().mockReturnValue('TestUser'),
 }));
