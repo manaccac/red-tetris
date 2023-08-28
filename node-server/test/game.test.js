ac141a85a63b999cf667d61a1b1281d3a6b698ef
manaccac <maximenaccache@gmail.com>
2023-08-28 15:59:31 +0200

    fini des test  76.31 |    56.66 |   75.55 |   76.57 |

---
+++ node-server/test/game.test.js
@@ -0,0 +1,92 @@
+const Game = require('../src/game');
+const Player = require('../src/player');
+
+jest.mock('../src/player'); // Mocking the Player class
+
+const socketMock = {
+    id: '1',
+    rooms: new Set(),
+    leave: jest.fn(),
+    emit: jest.fn(),
+    join: jest.fn(),
+    rooms: new Set(['room1', 'room2']),
+    broadcast: {
+        to: jest.fn().mockReturnThis(),
+        emit: jest.fn()
+    }
+};
+
+const { games, players } = require('../src/gameState');
+
+describe('Game class functions', () => {
+    let game;
+
+    beforeEach(() => {
+        // Set up player data
+        players.set('1', { name: 'John' });
+
+        game = new Game(socketMock, 'gameMode');
+    });
+
+    test('addPlayer should add a player to the game', () => {
+        const playerData = { name: 'Alice', winScore: 0, image: 'image_path', socket: '1' };
+
+        
+        Player.mockImplementationOnce(() => ({
+            getName: () => 'Alice'
+            
+        }));
+
+        game.addPlayer(socketMock);
+
+        expect(game.players.length).toBe(2);
+    });
+
+    test('removePlayer should remove a player from the game', () => {
+        const playerData = { name: 'Alice', winScore: 0, image: 'image_path', socket: '1' };
+
+        
+        Player.mockImplementationOnce(() => ({
+            getName: () => 'Alice'
+            
+        }));
+
+        game.addPlayer(socketMock);
+    });
+
+	
+    test('getWinner should return a player if there is only one player left with gameOver false', () => {
+        const playerData = { name: 'Alice', winScore: 0, image: 'image_path', socket: '1' };
+
+        Player.mockImplementation(() => ({
+            getName: () => 'Alice',
+            gameOver: false,
+            role: 'player'
+        }));
+
+        game.addPlayer(socketMock);
+
+        const winner = game.getWinner();
+
+        expect(winner).toBe(null);
+
+        players.get('1').gameOver = true;
+    });
+
+    test('changeLeader should update the leader with a random player', () => {
+        const playerData = { name: 'Alice', winScore: 0, image: 'image_path', socket: '1' };
+
+        Player.mockImplementation(() => ({
+            getName: () => 'Alice'
+        }));
+
+        game.addPlayer(socketMock);
+        game.addPlayer(socketMock);
+
+        const initialLeader = game.leader;
+
+        game.changeLeader();
+
+        expect(game.leader).not.toBe(initialLeader);
+    });
+});
